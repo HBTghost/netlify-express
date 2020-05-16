5f4b8bc01c4956efd8b87299e0b440bca9f875f6
Brian Drake <hacbangtam@gmail.com>
2020-05-16 11:15:57 +0700

    Cors

--- src/api.js
+++ src/api.js
@@ -13,11 +13,23 @@ if (process.env.NODE_ENV === "dev") {
   app.use(morgan("dev"));
 }
 
+// Cors
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 // Lazy load routes
 // app.use("/api/genDateStrings", require("./routes/genDateStrings.js"))
 app.use("/.netlify/functions/api/result", require("./routes/result.js"));
 
-// app.use(`/.netlify/functions/api`, router);
+// Choose port
+const port = process.env.PORT || 7070;
+
+// Listen
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+})
 
 module.exports = app;
 module.exports.handler = serverless(app);
